ad1d41dbacc5a0291505f9835181c053af40fea6
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-06 14:59:14 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
 
-const mangodbUrl = "mongodb+srv://ashishsharma0t:j3ly1BJ4yG3v77pM@blogapi.lglb8sl.mongodb.net/?retryWrites=true&w=majority&appName=blogapi"
+const mangodbUrl = "mongodb+srv://ashishsharma0t:j3ly1BJ4yG3v77pM@blogapi.tymk07d.mongodb.net/?retryWrites=true&w=majority&appName=blogapi"
 
 const connectDb = () => {
     return mongoose.connect(mangodbUrl);
