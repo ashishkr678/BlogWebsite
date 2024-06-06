e212a7db08724acc0a1073890c63af61c0126991
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-06 11:55:43 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,5 @@
 const app = require(".");
 const { connectDb } = require("./config/db");
-const path = require("path");
-
-app.get("/",(req,res) => {
-    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-})
 
 const PORT = 5700;
 app.listen(PORT, async() => {
