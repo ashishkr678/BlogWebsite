c034d83772f92d8d37ea57e8412fa692079d744d
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-06 12:25:04 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,7 +9,7 @@ const Home = ({setIsLoggedIn}) => {
 
   const fetchPosts = async () => {
     try {
-      const res = await axios.get(`http://localhost:5700/api/posts/`);
+      const res = await axios.get(`https://blog-api-vnos.onrender.com/api/posts/`);
       setPosts(res.data);
       res.data.length > 0 ? setHasMore(true) : setHasMore(false);
       setIsLoggedIn(true);
