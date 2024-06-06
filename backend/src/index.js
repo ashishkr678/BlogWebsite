7992cd7347cdbaaca94998cd8cf84cfb06751967
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-06 12:03:16 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,18 @@
 const express = require("express")
 
 const cors = require("cors")
+const { connectDb } = require("./config/db");
 
 const app = express()
+const PORT = 5700;
+app.listen(PORT, async() => {
+    await connectDb();
+    console.log("E-Commerce api listing on PORT : ",PORT);
+})
 
 app.use(express.json())
-app.use(cors(
-    origin: "https://blog-pearl-kappa.vercel.app"
-));
+app.use(cors());
+
 
 app.get("/",(req,res) => {
     return res.status(200).send({message : "welcome to StudyNotion",status:true})
