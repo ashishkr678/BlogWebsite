eb1a128b01a43840d0c853820999d612f4611294
Ashish Kumar <ashishkumat818@gmail.com>
2024-06-06 12:03:29 +0530

    Delete backend/src/server.js

--- backend/src/server.js
+++
@@ -1,8 +0,0 @@
-const app = require(".");
-const { connectDb } = require("./config/db");
-
-const PORT = 5700;
-app.listen(PORT, async() => {
-    await connectDb();
-    console.log("E-Commerce api listing on PORT : ",PORT);
-})
